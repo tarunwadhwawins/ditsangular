467816ec201c4a49110d909fd068dfcc5435760a
tarun wadhwa <tarunwadhwawins@gmail.com>
2021-09-07 16:16:20 +0530

    ui changes

--- src/app/ui-component/ui-component.module.ts
+++ src/app/ui-component/ui-component.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
 
 
 
@@ -7,6 +9,13 @@ import { CommonModule } from '@angular/common';
   declarations: [],
   imports: [
     CommonModule,
+    NzGridModule,
+    NzLayoutModule
+  ],
+  exports: [
+    CommonModule,
+    NzGridModule,
+    NzLayoutModule
   ]
 })
 export class UiComponentModule { }
